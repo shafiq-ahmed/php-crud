f108ac44ea1b62871b713cef81e648838f002ad5
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-08-28 09:33:41 +0600

    Form validated

--- create.php
+++ create.php
@@ -5,13 +5,26 @@
 
 $pdo= new PDO('mysql:host=localhost;port=3306;dbname=products_crud','root','');
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-var_dump($_SERVER);
+
+$errors=[];
+$title='';
+$price='';
+$description='';
+
 if ($_SERVER['REQUEST_METHOD']==='POST'){
 $title= $_POST['title'];
 $description= $_POST['desc'];
 $price= $_POST['price'];
 $date=date('Y-m-d H:i:s');
 
+if(!$title){
+  $errors[]="Product title is required";
+}
+if(!$price){
+  $errors[]="Product price is required";
+}
+
+if(empty($errors)){
 $statement=$pdo->prepare("INSERT INTO products(title,description,image,price,create_date) VALUES ( :title, :description,:image,:price,:date)");
 $statement->bindValue(':title',$title);
 $statement->bindValue(':image','');
@@ -19,6 +32,7 @@ $statement->bindValue(':description',$description);
 $statement->bindValue(':price',$price);
 $statement->bindValue(':date',$date);
 $statement->execute();
+}
 }
 ?>
 
@@ -37,16 +51,22 @@ include 'style.css';
 
 <div class="create-page">
   <div class="form">
-    
+    <?php if(!empty($errors)):?>
+    <div class="error">
+      <?php foreach($errors as $error):?>
+        <div><?php echo $error;?></div>
+      <?php endforeach; ?>
+    </div>
+    <?php endif;?>
     <form action="create.php" method="post" class="create-form">
       <label>Product Image</label>
-      <input type="file" name="image"/>
+      <input type="file" name="image" />
       <label>Product Title</label>
-      <input type="text" name="title"/>
+      <input type="text" name="title" value="<?php echo $title?>"/>
       <label>Description</label>
-      <textarea name="desc"></textarea>
+      <textarea name="desc" ><?php echo $description?></textarea>
       <label>Price</label>
-      <input type="number" step=".01" name="price"/>
+      <input type="number" step=".01" name="price" value="<?php echo $price?>"/>
       <button  type="submit" value="submit">Create</button>
     </form>
   </div>
